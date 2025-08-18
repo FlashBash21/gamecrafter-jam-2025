3202af32c74532ef828c6b6073ae04481c72a32e
FlashBash21 <nathanolson55379@gmail.com>
2025-08-17 21:51:27 -0500

    Add Drag-n-Drop Cards
    Created blank cards that can be picked up and moved around with the mouse.

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,16 +1,64 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private SpriteRenderer _spriteRenderer;
+    private bool _isSelected = false;
+    private bool _isDragging = false;
+
+    private Vector3 offset;
+
+    private void Update()
     {
-        
+        if (_isSelected)
+        {
+            _spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Card-Hover");
+        }
+        else
+        {
+            _spriteRenderer.sprite = Resources.Load<Sprite>("Sprites/Blank-Card");
+        }
+
+        if (_isDragging)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) + offset;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SelectCard()
     {
-        
+        _isSelected = true;
+    }
+
+    private void DeselectCard()
+    {
+        _isSelected = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        SelectCard();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        DeselectCard();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        offset = transform.position - Camera.main.ScreenToWorldPoint(eventData.position);
+        Debug.Log(transform.InverseTransformPoint(eventData.position));
+        Debug.Log(transform.position.xy());
+        _isDragging = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _isDragging = false;
     }
 }
